6017e1f61d70edf232faa40918237c2f90a66ffe
xBryan-iii <52694543+xBryan-iii@users.noreply.github.com>
2019-08-22 11:50:27 +0300

    First Commit

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ client.on('message', message => {
         case 'send':
             if (!message.content.startsWith(PREFIX)) return
             if (!args[1]) return message.reply('Error occurred! Please type your message.')
-            message.guild.channels.find(channel => channel.id === args[1]).send(message.content.split(" ").slice(1).join(" ").slice())
+            message.guild.channels.find(channel => channel.id === args[1]).send(message.content.split(" ").slice(2).join(" ").slice())
             break;
         case 'clear':
             if (!message.content.startsWith(PREFIX)) return
