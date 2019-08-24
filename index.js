07a5572229b435d8f378017efde442b81d09ec77
xBryan-iii <52694543+xBryan-iii@users.noreply.github.com>
2019-08-24 19:39:12 +0300

    First Commit

--- index.js
+++ index.js
@@ -11,6 +11,79 @@ client.on('ready', () =>{
 
 });
 
+client.on('raw', event => {
+    console.log(event)
+    const eventName = event.t;
+    if (eventName === 'MESSAGE_REACTION_ADD')
+    {
+        if (event.d.message_id === '614851498731503663')
+        {
+            var reactionChannel = client.channels.get(event.d.channel_id);
+            if (reactionChannel.messages.has(event.d.message_id))
+                return;
+            else {
+                reactionChannel.fetchMessage(event.d.message_id)
+                .then (msg => {
+                    var msgReaction = msg.reactions.get(event.d.emoji.name + ":" + event.d.emoji.id);
+                    var user = client.users.get(event.d.user_id)
+                    client.emit('messageReactionAdd', msgReaction, user);
+                })
+                .catch(err => console.log(err));
+            }
+        }
+    }
+    else if (eventName === 'MESSAGE_REACTION_REMOVE')
+    {
+        if (event.d.message_id = '614851498731503663')
+        {
+            var reactionChannel = client.channels.get(event.d.channel_id);
+            if (reactionChannel.messages.has(event.d.message_id))
+                return;
+            else {
+                reactionChannel.fetchMessage(event.d.message_id)
+                .then (msg => {
+                    var msgReaction = msg.reactions.get(event.d.emoji.name + ":" + event.d.emoji.id);
+                    var user = client.users.get(event.d.user_id)
+                    client.emit('messageReactionRemove', msgReaction, user);
+                })
+                .catch(err => console.log(err));
+            }
+        }
+    }
+});
+
+client.on('messageReactionAdd', (messageReaction, user) => {
+    var roleName = messageReaction.emoji.name;
+    var role = messageReaction.message.guild.roles.find(role => role.name.toLowerCase() ===
+    roleName.toLowerCase());
+
+    if (role)
+    {
+        var member = messageReaction.message.guild.members.find(member => member.id === user.id);
+        if (member)
+        {
+            member.addRole(role.id);
+            console.log("Success. Added role.");
+        }
+    }
+});
+
+client.on('messageReactionRemove', (messageReaction, user) => {
+    var roleName = messageReaction.emoji.name
+    var role = messageReaction.message.guild.roles.find(role => role.name.toLowerCase() ===
+    roleName.toLowerCase());
+
+    if (role)
+    {
+        var member = messageReaction.message.guild.members.find(member => member.id === user.id);
+        if (member)
+        {
+            member.removeRole(role.id);
+            console.log("Success. Removed role.");
+        }
+    }
+});
+
 client.on('message', message => {
     
     let args = message.content.slice(PREFIX.length).split(/ +/);
