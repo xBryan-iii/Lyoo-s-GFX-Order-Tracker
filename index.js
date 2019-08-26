6612774f20b7c9e47f1d466f1260f59ed9cf0029
xBryan-iii <52694543+xBryan-iii@users.noreply.github.com>
2019-08-26 13:41:53 +0300

    First Commit

--- index.js
+++ index.js
@@ -10,10 +10,7 @@ client.on('ready', () =>{
     client.user.setActivity(`Orders`, { type: 'WATCHING'}).catch(console.error);
     let myGuild = client.guilds.get('573082577288822805');
     let memberCount = myGuild.memberCount;
-    let userCountChannel = myGuild.channels.get('615485514501455872');
-    let botCountChannel = myGuild.channels.get('615485353192587274');
     let memberCountChannel = myGuild.channels.get('615073428977745930');
-    userCountChannel.setName('Total Users: ' + memberCount.valueOf(client))
     memberCountChannel.setName('Total Members: ' + memberCount)
     .then(result => console.log(result))
     .catch(error => console.log(error));
@@ -22,11 +19,7 @@ client.on('ready', () =>{
 client.on('guildMemberAdd', member => {
     let myGuild = client.guilds.get('573082577288822805');
     let memberCount = myGuild.memberCount;
-    let userCountChannel = myGuild.channels.get('615485514501455872');
-    let botCountChannel = myGuild.channels.get('615485353192587274');
     let memberCountChannel = myGuild.channels.get('615073428977745930');
-    userCountChannel.setName('Total Users: ' + memberCount.valueOf(bot))
-    botCountChannel.setName('Total Bots ' + member.bot)
     memberCountChannel.setName('Total Members: ' + memberCount)
     .then(result => console.log(result))
     .catch(error => console.log(error));
@@ -35,11 +28,7 @@ client.on('guildMemberAdd', member => {
 client.on('guildMemberRemove', member => {
     let myGuild = client.guilds.get('573082577288822805');
     let memberCount = myGuild.memberCount;
-    let userCountChannel = myGuild.channels.get('615485514501455872');
-    let botCountChannel = myGuild.channels.get('615485353192587274');
     let memberCountChannel = myGuild.channels.get('615073428977745930');
-    userCountChannel.setName('Total Users: ' + memberCount.valueOf(client))
-    botCountChannel.setName('Total Bots: ' + member.bot)
     memberCountChannel.setName('Total Members: ' + memberCount)
     .then(result => console.log(result))
     .catch(error => console.log(error));
